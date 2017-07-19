3307b9790f8d966979bb49d9171fa8b1f7c51119
Viktor Yanush <yanushviktor@gmail.com>
2017-07-19 12:21:52 +0000

    fixed bug with input shape

--- optimizees/quadratic.py
+++ optimizees/quadratic.py
@@ -24,9 +24,8 @@ class Quadratic(optimizee.Optimizee):
     
     def loss(self, x, i):
         b = tf.expand_dims(self.b, -1)
-        x = tf.expand_dims(x, -1)
 
-        f = (tf.matmul(self.W, x) - b)**2
+        f = (tf.matmul(self.W, tf.expand_dims(x, -1)) - b)**2
         f = tf.reduce_mean(tf.squeeze(f, axis=-1), axis=-1)
         g = self.grad(x, f)
         return f, g
